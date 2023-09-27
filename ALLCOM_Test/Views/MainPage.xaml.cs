e6d205caf16e97a2468924b164a3e75ecd24e921
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2023-09-27 18:03:56 +0800

    m

--- ALLCOM_Test/Views/MainPage.xaml.cs
+++ ALLCOM_Test/Views/MainPage.xaml.cs
@@ -106,8 +106,21 @@ public class DataListDatas : ObservableObject
 
     public DataListDatas(string dataName, double data, string id)
     {
- 
-        DataColor = new SolidColorBrush(Colors.RoyalBlue); 
+
+        //DataColor = new SolidColorBrush(Colors.Salmon);
+        DataColor = new SolidColorBrush(Colors.RoyalBlue);
+        DataName = dataName;
+        ID = id;
+        Data = data;
+        var deleteCommand = new StandardUICommand(StandardUICommandKind.Stop);
+        // deleteCommand.ExecuteRequested += DeleteCommand_ExecuteRequested;
+        Command = deleteCommand;
+        is_View = true;
+    }
+    public DataListDatas(string dataName, double data, string id, Color color)
+    {
+        //DataColor = new SolidColorBrush(Colors.Salmon);
+        DataColor = new SolidColorBrush(color);
         DataName = dataName;
         ID = id;
         Data = data;
@@ -137,12 +150,88 @@ public sealed partial class MainPage : Page
         get;
     }
 
+
+
+
+
+
+
+    Color ColorFromHSV(double hue, double saturation, double value)
+    {
+        double chroma = value * saturation;
+        double huePrime = hue / 60.0;
+        double x = chroma * (1 - Math.Abs(huePrime % 2 - 1));
+
+        double red = 0, green = 0, blue = 0;
+
+        if (huePrime >= 0 && huePrime <= 1)
+        {
+            red = chroma;
+            green = x;
+        }
+        else if (huePrime > 1 && huePrime <= 2)
+        {
+            red = x;
+            green = chroma;
+        }
+        else if (huePrime > 2 && huePrime <= 3)
+        {
+            green = chroma;
+            blue = x;
+        }
+        else if (huePrime > 3 && huePrime <= 4)
+        {
+            green = x;
+            blue = chroma;
+        }
+        else if (huePrime > 4 && huePrime <= 5)
+        {
+            red = x;
+            blue = chroma;
+        }
+        else if (huePrime > 5 && huePrime <= 6)
+        {
+            red = chroma;
+            blue = x;
+        }
+
+        double m = value - chroma;
+
+        red += m;
+        green += m;
+        blue += m;
+
+        byte redByte = (byte)(red * 255);
+        byte greenByte = (byte)(green * 255);
+        byte blueByte = (byte)(blue * 255);
+
+        return Color.FromArgb(255, redByte, greenByte, blueByte);
+    }
+
+    public Color GenerateDistinctColor(int number)
+    {
+        if (number < 0 || number > 127)
+        {
+           number = 127;
+        }
+
+        double hue = (number * 2.83) % 360; // 将数字映射到色轮上
+
+        return ColorFromHSV(hue, 1, 1);
+    }
+
+
+
+
+
+
     public MainPage()
     {
         ViewModel = App.GetService<MainViewModel>();
         for (int i = 0; i < 20; i++)
         {
             dateSource.Add(new DataListDatas("data"+ i.ToString(), 0, i.ToString()));
+            dateSource[i].DataColor.Color = GenerateDistinctColor((i*78)%128);
         }
 
         InitializeComponent();
@@ -269,5 +358,13 @@ public sealed partial class MainPage : Page
             var str = textBox.Tag as string;
             dateSource[Convert.ToInt32(str)].DataName = textBox.Text;
         }
+    }
+
+    private void myColorPicker_ColorChanged(ColorPicker sender, ColorChangedEventArgs args)
+    {
+        var str = sender.Tag as string;
+        dateSource[Convert.ToInt32(str)].DataColor.Color = sender.Color;
+        //dateSource[Convert.ToInt32(str)].DataColor = new SolidColorBrush(sender.Color);
+
     }
 }
